84df2c5d351af7c972cdb1e93c89b149de73a183
Zeey76 <abdulrasaqzainab76@gmail.com>
2025-01-12 18:34:03 +0100

    .

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import FaqContent from "./FaqContent";
+
 function Faq() {
+  const [openPanel, setOpenPanel] = useState(null)
+  function handlePanel (index) {
+    setOpenPanel((prev) => (prev === index ? null : index))
+  }
   return (
     <div className="flex flex-col mt-6 gap-3 justify-center items-center w-[80%] mx-auto">
       <div className="max-w-[28rem] flex justify-center flex-col">
@@ -15,31 +22,45 @@ function Faq() {
           <hr />
           <div className="flex justify-between items-center mt-1 mb-1">
             <h4>What is a Bookmark?</h4>
+           
             <div>
-              <img src="./images/icon-arrow.svg" />
+              <button onClick={() =>  handlePanel(1)}>
+              {openPanel !== 1 ? <img src="./images/icon-arrow.svg" /> : <img src="./images/icon-close.svg" />}
+              </button>
+              
             </div>
           </div>
+          {openPanel === 1 && <FaqContent/>}
           <hr />
           <div className="flex justify-between items-center mt-1 mb-1">
             <h4>How can i request a new browser?</h4>
             <div>
+            <button onClick={() =>  handlePanel(2)}>
               <img src="./images/icon-arrow.svg" />
+              </button>
             </div>
           </div>
+          {openPanel === 2 && <FaqContent/>}
           <hr />
           <div className="flex justify-between items-center mt-1 mb-1">
             <h4>Is there a mobile app?</h4>
             <div>
+            <button onClick={() =>  handlePanel(3)}>
               <img src="./images/icon-arrow.svg" />
+              </button>
             </div>
           </div>
+          {openPanel === 3 && <FaqContent/>}
           <hr />
           <div className="flex justify-between items-center mt-1 mb-1">
             <h4>What about other Chromium browsers</h4>
             <div>
+            <button onClick={() =>  handlePanel(4)}>
               <img src="./images/icon-arrow.svg" />
+              </button>
             </div>
           </div>
+          {openPanel === 4 && <FaqContent/>}
           <hr />
         </div>
         <button className="self-center mt-3 bg-SoftBlue w-10 pt-0.5 pb-0.5 text-white rounded-sm">
---
+++ src/components/FaqContent.js
@@ -0,0 +1,13 @@
+export default function FaqContent() {
+  return (
+    <div className="pt-1 pb-1 block">
+      <p className="leading-loose">
+        looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt trfghjku tyjhkui bytthj
+        tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt trfghjku tyjhkui
+        bytthj tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt trfghjku
+        tyjhkui bytthj tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt
+        trfghjku tyjhkui bytthj tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui
+      </p>
+    </div>
+  );
+}
