bb4faffe224d7bbba2d832be50105353f62127cf
Zeey76 <abdulrasaqzainab76@gmail.com>
2025-01-13 17:25:36 +0100

    updated styling

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -8,65 +8,133 @@ function Faq() {
   }
   return (
     <div className="flex flex-col mt-6 gap-3 justify-center items-center w-[80%] mx-auto">
-      <div className="max-w-[28rem] flex justify-center flex-col">
+      <div className="max-w-[30rem] flex justify-center flex-col">
         <div className="mb-2">
           <h2 className="text-3xl text-center mb-1 font-bold text-VeryDarkBlue">
             Frequently Asked Questions
           </h2>
-          <p className="text-GrayishBlue text-center">
+          <p className="text-GrayishBlue text-center text-[1.1rem]">
             Here are some of our FAQs. If you have any other questions you'd
             like answered please feel free to email us.
           </p>
         </div>
         <div>
           <hr />
-          <div className="flex justify-between items-center mt-1 mb-1">
-            <h4 className="font-semibold text-VeryDarkBlue">What is a Bookmark?</h4>
+          <div className="flex justify-between items-center mt-1 mb-1 cursor-pointer"  onClick={() => handlePanel(1)}>
+            <h4 className="font-semibold text-VeryDarkBlue text-[1.1rem]">
+              What is a Bookmark?
+            </h4>
 
             <div>
-              <button onClick={() => handlePanel(1)}>
+              <button>
                 {openPanel !== 1 ? (
                   <img src="./images/icon-arrow.svg" alt="" />
                 ) : (
-                  <img src="./images/icon-close.svg" alt="" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="rotate-180"
+                    width="18"
+                    height="12"
+                  >
+                    <path
+                      fill="none"
+                      stroke="hsl(0, 94%, 66%)"
+                      stroke-width="3"
+                      d="M1 1l8 8 8-8"
+                    />
+                  </svg>
                 )}
               </button>
             </div>
           </div>
           {openPanel === 1 && <FaqContent />}
           <hr />
-          <div className="flex justify-between items-center mt-[0.8rem] mb-[0.8rem]">
-            <h4 className="font-semibold text-VeryDarkBlue">How can i request a new browser?</h4>
+          <div className="flex justify-between items-center mt-1 mb-1 cursor-pointer"  onClick={() => handlePanel(2)}>
+            <h4 className="font-semibold text-VeryDarkBlue text-[1.1rem]">
+              How can i request a new browser?
+            </h4>
             <div>
-              <button onClick={() => handlePanel(2)}>
-                <img src="./images/icon-arrow.svg" alt="" />
+              <button>
+                {openPanel !== 2 ? (
+                  <img src="./images/icon-arrow.svg" alt="" />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="rotate-180"
+                    width="18"
+                    height="12"
+                  >
+                    <path
+                      fill="none"
+                      stroke="hsl(0, 94%, 66%)"
+                      stroke-width="3"
+                      d="M1 1l8 8 8-8"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
           {openPanel === 2 && <FaqContent />}
           <hr />
-          <div className="flex justify-between items-center mt-[0.8rem] mb-[0.8rem]">
-            <h4 className="font-semibold text-VeryDarkBlue">Is there a mobile app?</h4>
+          <div className="flex justify-between items-center mt-1 mb-1 cursor-pointer"  onClick={() => handlePanel(3)}>
+            <h4 className="font-semibold text-VeryDarkBlue text-[1.1rem]">
+              Is there a mobile app?
+            </h4>
             <div>
-              <button onClick={() => handlePanel(3)}>
-                <img src="./images/icon-arrow.svg" alt="" />
+              <button>
+                {openPanel !== 3 ? (
+                  <img src="./images/icon-arrow.svg" alt="" />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="rotate-180"
+                    width="18"
+                    height="12"
+                  >
+                    <path
+                      fill="none"
+                      stroke="hsl(0, 94%, 66%)"
+                      stroke-width="3"
+                      d="M1 1l8 8 8-8"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
           {openPanel === 3 && <FaqContent />}
           <hr />
-          <div className="flex justify-between items-center mt-[0.8rem] mb-[0.8rem]">
-            <h4 className="font-semibold text-VeryDarkBlue">What about other Chromium browsers</h4>
+          <div className="flex justify-between items-center mt-1 mb-1 cursor-pointer"  onClick={() => handlePanel(4)}>
+            <h4 className="font-semibold text-VeryDarkBlue text-[1.1rem]">
+              What about other Chromium browsers
+            </h4>
             <div>
-              <button onClick={() => handlePanel(4)}>
-                <img src="./images/icon-arrow.svg" alt="" />
+              <button>
+                {openPanel !== 4 ? (
+                  <img src="./images/icon-arrow.svg" alt="" />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="rotate-180"
+                    width="18"
+                    height="12"
+                  >
+                    <path
+                      fill="none"
+                      stroke="hsl(0, 94%, 66%)"
+                      stroke-width="3"
+                      d="M1 1l8 8 8-8"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
           {openPanel === 4 && <FaqContent />}
           <hr />
         </div>
-        <button className="self-center mt-3 bg-SoftBlue w-10 pt-0.5 pb-0.5 text-white rounded-sm">
+        <button className="self-center mt-3 bg-SoftBlue w-8 rounded-md pt-[0.8rem] pb-[0.8rem] text-sm font-medium text-white shadow-md">
           More Info
         </button>
       </div>
--- src/components/FaqContent.js
+++ src/components/FaqContent.js
@@ -1,12 +1,13 @@
 export default function FaqContent() {
   return (
-    <div className="pt-1 pb-1 block">
-      <p className="leading-loose">
-        looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt trfghjku tyjhkui bytthj
-        tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt trfghjku tyjhkui
-        bytthj tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt trfghjku
-        tyjhkui bytthj tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui tfghjkuyt
-        trfghjku tyjhkui bytthj tfgfhh looyggvbtyfgv ftrghjutgf tfghjkui
+    <div className="pt-[0.6rem] pb-[1.4rem] block">
+      <p className="leading-loose text-gray-500 font-medium">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut eveniet
+        provident recusandae quaerat. Recusandae possimus corrupti culpa illo
+        explicabo aperiam ullam, assumenda a asperiores itaque ab iusto
+        voluptatibus perspiciatis officia, nostrum aliquam dolore debitis
+        veritatis libero? Labore enim fugiat asperiores dicta, laudantium illo
+        architecto, ab molestias placeat atque soluta totam?
       </p>
     </div>
   );
